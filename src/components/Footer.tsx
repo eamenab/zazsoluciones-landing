563ef9d40c3be33e8fd2e140000679f405b11b5d
ernestoamenabar <ernestoamenabar@gmail.com>
2025-03-25 12:33:09 -0300

    logo en dark

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,8 +9,8 @@ const Footer = () => {
       <div className="container mx-auto px-4 md:px-6">
         <div className="flex flex-col items-center justify-center mb-12">
           <div className="flex items-center mb-4">
-            <span className="text-2xl font-bold bg-blue-gradient highlight-text">Zaz</span>
-            <span className="ml-1 text-lg font-medium">Soluciones</span>
+            <span className="text-2xl font-bold highlight-text">Zaz</span>
+            <span className="ml-1 text-lg font-medium dark:text-black">Soluciones</span>
           </div>
           <p className="text-zaz-dark-gray mb-6 max-w-md text-center">
             Soluciones SaaS a la medida con inteligencia artificial para optimizar 
